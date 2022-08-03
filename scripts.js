165abcbbc75aa37cd2486f849a78220d7afe6d7e
Renzo Silva <renzoasilva@gmail.com>
2022-08-03 17:19:15 -0400

    first html project

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const menuElement = document.querySelector('#menu')
+
+fetch('https://codice-boca.web.app/menu')
+  .then(res => res.json())
+  .then(menuItems => {
+    let html = ''
+
+    menuItems.forEach((recipe) => {
+        let htmlSegment = `<div class="menu-item">
+        <h3>${recipe.title}</h3>
+        <p>${recipe.description}</p>
+        </div>`
+
+        html += htmlSegment
+    })
+
+    menuElement.innerHTML = html
+  })
+
+  .catch(err => console.error(err))
